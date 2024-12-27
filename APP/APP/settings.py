eea61070d330722964bb7ccf1430da9becda5e9c
ayano-yuki <ac234704@edu.okinawa-ct.ac.jp>
2024-12-27 09:01:45 +0900

    update: 日記を書ける機能の追加

--- APP/APP/settings.py
+++ APP/APP/settings.py
@@ -51,7 +51,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'APP.wsgi.application'
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
